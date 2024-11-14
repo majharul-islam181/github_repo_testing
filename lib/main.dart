50cf8868b6c47fb4dc434cac5011ff64fe9d5d1f
Md. Majharul Islam <86792533+majharul-islam181@users.noreply.github.com>
2024-11-14 16:32:22 +0530

    changed in branch2

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
+}
+
+class MyHomePage {
+  const MyHomePage();
 }
